228e20312a6ac3dcf4f2bdd0b1e0649e161a6624
Sofía <chofifotos@gmail.com>
2024-10-03 10:29:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
 st.title("¡Deja volar tu imaginación y dibuja!")
-drawing_mode = "freedraw"
-stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+
 stroke_color = '#FFFFFF' # Set background color to white
 bg_color = '#000000'
+
+with st.sidebar:
+    stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+    drawing_mode = st.selectbox(
+    "How would you like to be contacted?",
+    ("freedraw", "line", "rect"),
+)
+    
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
