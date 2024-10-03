7b196007f4b17d71118db1b5bd7fcc31fb771d5e
Sofía <chofifotos@gmail.com>
2024-10-03 09:57:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 st.title("¡Deja volar tu imaginación y dibuja!")
+drawing_mode = "freedraw"
+stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+stroke_color = '#FFFFFF' # Set background color to white
+bg_color = '#000000'
 canvas_result = st_canvas(
     fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
     stroke_width=stroke_width,
