aaef042e47b11b053c43cda0adfd10267d28419b
Sofía <chofifotos@gmail.com>
2024-10-03 09:56:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import streamlit as st
 st.title("¡Deja volar tu imaginación y dibuja!")
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    height=200,
+    width=200,
+    key="canvas",
+)
